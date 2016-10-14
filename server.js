cfb4bb949f253cf1e345e5ae8a9f5e249743dcc9
Jack Mocherman <mochermanjack@gmail.com>
2016-10-14 14:25:19 -0500

    added get request for all users and all items

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 'use strict'
 
 const express = require('express')
+const config = require('./knexfile').development
+const knex = require('knex')(config)
 
 const port = process.env.PORT || 3000
 const app = express() 
 
 app.set('port', port)
+
+app.get('/api/allUsers', (req, res) => {
+	knex('Users')
+		.select('*')
+		.then((data) => {
+			console.log(data)
+		})
+})
+
+app.get('/api/allItems', (req, res) => {
+	knex('Items')
+		.select('*')
+		.then((data) => {
+			console.log(data)
+		})
+})
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
