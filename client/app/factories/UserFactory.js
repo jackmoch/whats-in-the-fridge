32cb603935fdb8c223f65c3315843296caec7203
Jack Mocherman <mochermanjack@gmail.com>
2016-10-16 11:19:17 -0500

    put get all users http call in user factory

--- client/app/factories/UserFactory.js
+++ client/app/factories/UserFactory.js
@@ -1,13 +1,21 @@
 'use strict'
 
-app.factory('UserFactory', function($http, $scope) {
+app.factory('UserFactory', function($http, $q) {
 
-	const postUser = function(user) {
-		console.log(user)
+	const getAllUsers = function() {
+		return $q((resolve, reject) => 
+			$http
+				.get('/api/allUsers')
+				.then(({data}) => {
+					if (data) {
+						resolve(data)
+					} else {
+						reject(null)
+					}
+				})
+		)
 	}
 
-	console.log('test')
-
-	return { postUser }
+	return { getAllUsers }
 
 })
