7f3a62c5a0e9d0be97a7eddadb36160696f57343
Jack Mocherman <mochermanjack@gmail.com>
2016-10-16 11:43:22 -0500

    es6 syntax refactor

--- client/app/factories/UserFactory.js
+++ client/app/factories/UserFactory.js
@@ -2,25 +2,19 @@
 
 app.factory('UserFactory', function($http, $q) {
 
-	const getAllUsers = function() {
-		return $q((resolve, reject) => 
+	const getAllUsers = () => 
+		$q((resolve, reject) => 
 			$http
 				.get('/api/allUsers')
-				.then(({data}) => {
-					data ? resolve(data) : reject(null)
-				})
-		)
-	}
+				.then(({data}) => data ? resolve(data) : reject(null)))
+	
 
-	const postNewUser = function(newUser) {
-		return $q((resolve, reject) => 
+	const postNewUser = newUser => 
+		$q((resolve, reject) => 
 			$http
 			.post('/api/newUser', newUser)
-			.then((data) => {
-				data ? resolve() : reject() 
-			})
-		)
-	}
+			.then((data) => data ? resolve() : reject()))
+	
 
 	return { 
 		getAllUsers,
