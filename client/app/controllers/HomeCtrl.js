9996592147bd559bc5afd4bf8f219f49addffef5
Jack Mocherman <mochermanjack@gmail.com>
2016-10-15 19:58:40 -0500

    added user array that displays all users and runs get users function on submit, added links to users that show corresponding items when clicked

--- client/app/controllers/HomeCtrl.js
+++ client/app/controllers/HomeCtrl.js
@@ -2,14 +2,39 @@
 
 app.controller('HomeCtrl', function($scope, $http) {
 
+	$scope.userArray = []
+	$scope.itemArray = []
+
+	const getAllUsers = function() {
+
+		$http
+			.get('/api/allUsers')
+			.then(({data}) => {
+				$scope.userArray = data
+			})
+
+	}
+
+	getAllUsers()
+
 	$scope.submit = function(user) {
 
-		console.log(user)
+		$scope.userArray.push(user)
 
 		$http
 			.post('/api/newUser', user)
 			.then((data) => {
-				console.log(data)
+				getAllUsers()
+			})
+
+	}
+
+	$scope.showUserItems = function(id) {
+
+		$http
+			.get(`/api/getItems/${id}`)
+			.then(({data}) => {
+				$scope.itemArray = data
 			})
 
 	}
