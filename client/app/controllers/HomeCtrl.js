ad9491a52c95f42579a94b99915afd894d5e30e7
Jack Mocherman <mochermanjack@gmail.com>
2016-10-16 10:08:07 -0500

    added submit new item function and renamed submit function to submit user for clarity

--- client/app/controllers/HomeCtrl.js
+++ client/app/controllers/HomeCtrl.js
@@ -7,39 +7,34 @@ app.controller('HomeCtrl', function($scope, $http) {
 	$scope.currentUser = ''
 
 	const getAllUsers = function() {
-
 		$http
 			.get('/api/allUsers')
 			.then(({data}) => {
 				$scope.userArray = data
 			})
-
 	}
-
 	getAllUsers()
 
-	$scope.submit = function(user) {
-
+	$scope.submitUser = function(user) {
 		$scope.userArray.push(user)
-
 		$http
 			.post('/api/newUser', user)
 			.then((data) => {
 				getAllUsers()
 			})
-
 	}
 
 	$scope.showUserItems = function(user) {
-
 		$scope.currentUser = user
-
 		$http
 			.get(`/api/getItems/${user.id}`)
 			.then(({data}) => {
 				$scope.itemArray = data
 			})
+	}
 
+	$scope.submitNewItem = function(newItem) {
+		console.log(newItem)
 	}
 
 })
